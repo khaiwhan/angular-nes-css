51817cde1033e3623ac385ea086939cd08f5bbce
khaiwhan <sritonchai.p@gmail.com>
2024-08-29 15:21:54 +0700

    remove cache

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { prefixInterceptor } from './prefix.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(),
+    provideRouter(routes),
     provideHttpClient(
       withInterceptors([prefixInterceptor]),
       withFetch()
